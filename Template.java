e6aa61516d6f2ba53a7099dd881b04e69d68e702
Johnny Piette <johnny.piette@gmail.com>
2022-09-13 13:59:12 +0200

    Updates

--- Template.java
+++ Template.java
@@ -1,28 +1,4 @@
 public class Template {
-    // Les programmes JAVA que vous avez faits pour le moment ne demandaient pas de paramètres pour être exécutés.
-    // Et vous les exécutez de la manière suivante: java programme.java
-    //
-    // Si vous aviez besoin d'une information de l'utilisateur, vous lui demandiez via un scanner.
-    //
-    // Cependant, votre programme pourrait ne pas être utilisé par un humain (script: automatisation de tâches).
-    // Il pourrait aussi être utilisé en tant qu'API que d'autres programmeurs pourrait utiliser.
-    // Et là, on passerait des variables en paramètres à votre programme au lancement de celui-ci.
-    //
-    //
-    // String[] args signifie que args est un tableau de String: c'est à dire un tableau contenant différentes chaînes de caractères
-    // Un tableau a une taille qui représente le nombre d'éléments.
-    //
-    // Pour connaître la taille du tableau  args on utilisera args.length
-    //
-    // Si on exécute notre programme de cette manière: java Template.java add 12 45
-    //
-    // Notre tableau contiendra les 3 String suivantes: "add" "12" "45".
-    // On accédera aux différentes String de notre tableau de cette manière à l'aide de crochets et d'un entier:
-    // args[0]   on accède à la chaîne stockée à l'indice 0 du tableau: "add" Un tableau commence toujours à l'indice 0.
-    // args[1]   on accède à la chaîne stockée à l'indice 1 du tableau: "12"
-    // args[2]   on accède à la chaîne stockée à l'indice 2 du tableau: "45"
-    // args[3]   on essaie d'accèder à la chaîne stockée à l'indice  du tableau: Cela va provoquer une erreur car notre tableau ne contient que 3 élèments.
-
     public static void main(String[] args) {
         // On vérifie qu'on le tableau a bien reçu des paramètres: c'est à dire est-ce
         // que notre tableau à plus que zéro élément ?
